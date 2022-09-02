5f9a4971ad65ca2365e0cdb696d95a5a5d637e7e
Mansour <mansourrahimi008@gmial.com>
2022-09-02 22:29:59 +0430

    All is Added To Git

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var expresslayout = require('express-ejs-layouts');
+var indexRouter = require('./routes/index')
+
+
+
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.set('layout' , './layouts/webLayout')
+app.set('layout register', false)
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(expresslayout)
+app.set(express.urlencoded({extended: false}))
+app.set(express.json());
+app.use('/' , indexRouter)
+
+
+//portNumber
+app.listen(3000)
+module.exports = app
